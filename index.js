5bef8bb8507f5b5f7fc78f0f8719220e177fe6b6
negar miralaie <miralaienegar@gmail.com>
2022-09-14 01:09:44 +0430

    competed rate limit

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-app.use(cors());
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 5 // users can make 5 requests in 10 minutes
+});
 
+const PORT = process.env.PORT || 3000;
+
+app.use(limiter);
+app.set('trust proxy', 1);
+app.use(cors());
 
 app.use('/api', require('./routes/index'));
 
